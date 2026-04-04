63e9422aa588bf2a407a6f470e4c253f38a950cf
Asif Sayyed <asifdotexe@gmail.com>
2026-04-04 12:19:35 +0530

    #1 feat: add internal function for running cmd

---
+++ scripts/analyse_repository.py
@@ -0,0 +1,30 @@
+"""
+This script is reposible for doing the heavy lifting.
+Collects the 12 blames per year for target repository
+"""
+
+import subprocess
+
+
+def _run_command(cmd: list[str], cwd: str) -> str:
+    """
+    Execute a shell command and return it's standard output
+
+    :param cmd: List of arguments forming the command.
+    :param cwd: Directory path where the command should be executed.
+    :return: Decoded standard output of the command.
+    """
+    try:
+        result = subprocess.run(
+            cmd,
+            cwd=cwd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            check=True,
+        )
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(
+            f"Command '{' '.join(cmd)}' failed with exit code {e.returncode}"
+        ) from e
